1abd0aec04e2778d3cb5395a67d9d305100e9171
Dick Wall <dick@Pegasus.(none)>
2010-04-08 13:51:12 -0700

    Initial commit

---
+++ src/Haplotyper.scala
@@ -0,0 +1,152 @@
+import collection.mutable.ListBuffer
+
+/**
+ * Identify and optimize haplotypes
+ */
+
+class Haplotyper {
+
+  /**
+   * Used to ensure matching length on any two sequences or lists that need to be the same length to be zipped.
+   * Note: structural typing used - as long as the object answers to a length property, we can use it
+   * @param lengthyThing1 an instance some type that has a length property
+   * @param lengthyThing2 another instance of some type that has a length property
+   * @throws IllegalArgumentException if the instances have different length
+   *
+   */
+  def ensureMatchingLength(lengthyThing1 : {def length : Int}, lengthyThing2 : {def length : Int}) =
+    if (lengthyThing1.length != lengthyThing2.length)
+      throw new IllegalArgumentException("This method will not handle sequences of differing lengths")
+
+
+  /**
+   * Extract the mutation sets for a pair of sequences. The mutation sets are an array of sets of the possible
+   * mutations at any given point in the sequence. For many positions, this will be a single possible nucleotide
+   * but where mutations are indicated in the sequences, there will be more than one nucleotide in the set.
+   * @param seq1 a string sequence
+   * @param seq2 a second string sequence to compare to the first
+   * @return an array of set is  of chars with the possible nucleotides at each position in the sequence
+   */
+  def extractMutationSets(seq1 : String, seq2 : String) : Array[Set[Char]] = {
+    ensureMatchingLength(seq1, seq2)
+    val zipped = seq1.toArray zip seq2.toArray
+    zipped map { case (allele1, allele2) => Set(allele1, allele2) }
+  }
+
+  /**
+   * Given mutation sets obtained from either comparing sequence strings, or combining other mutation sets,
+   * merge two mutation sets together. This can be used as a big reduce function over an entire list of mutation sets.
+   * @param mset1 mutation set one (an array of sets of chars representing posssible nucleotides)
+   * @param mset2 as for mset1, the mutation set that will be merged in
+   * @return a new array of sets of chars with the possible nucleotides at each position in the sequence
+   */
+  def combineMutationSets(mset1 : Array[Set[Char]], mset2 : Array[Set[Char]]) = {
+    ensureMatchingLength(mset1, mset2)
+    val zipped = mset1 zip mset2
+    zipped map { case (set1, set2) => set1 ++ set2 }
+  }
+
+  /**
+   * For a list of sequence strings, first find all of the mutation sets by comparing all strings with a pair, then
+   * reduce the resulting list of sets by combining them to create new outer sets of possibilities for each location.
+   * @param sequences, the list of all possible sequences for a region of interest
+   * @return an array of sets of characters for each position in the sequence, along with the possible nucleotides there
+   */
+  def findAllMutations(sequences : List[String]) : Array[Set[Char]] = {
+    val (first : List[String], second : List[String]) = {
+      if (sequences.length % 2 == 0) (sequences.splitAt(sequences.length / 2))
+      else {
+        val firstItem = sequences.head
+        val rest = sequences.tail
+        val (first, second) = rest.splitAt(rest.length / 2)
+        (List(firstItem) ::: first, List(firstItem) ::: second)
+      }
+    }
+    val zipped = first zip second
+    val pairedMutations = zipped map { case(s1, s2) => extractMutationSets(s1, s2)}
+
+    if (pairedMutations == Nil) Array()
+    else pairedMutations reduceLeft { combineMutationSets(_,_) }
+  }
+
+  /**
+   * Given an array of possible nucleotides for each position in a sequence (the output from findAllMutations), return
+   * a map of the index of any position in that sequence that has observed mutations, along with the possible
+   * nucleotides. Positions with only one observed nucleotide will be excluded from the map.
+   * @param mutationList the Array of sets of nucleotides at each position in a sequence
+   * @return a map of the index of any locations with more than one possible nucleotide, to a set of nucleotides
+   * that can be at that position.
+   */
+  def mutationMap(mutationList : Array[Set[Char]]) = {
+    var mmap = Map[Int, Set[Char]]()
+
+    for (i <- 0 to (mutationList.length - 1)) {
+      if (mutationList(i).size > 1) mmap += i -> mutationList(i)
+    }
+
+    mmap
+  }
+
+  /**
+   * For a given sequence, along with an established mutation map for the nucleotides, return a list of Mutations
+   * for the position index number and the nucleotide in this specific sequence for the possible mutation locations.
+   */
+  def getHaplotype(sequence : String, mutationMap : Map[Int, Set[Char]]) = {
+    val chArray = sequence.toArray
+    val haplotypeList = new ListBuffer[Mutation]
+
+    for ((posn, possibles) <- mutationMap) {
+      // check that the nucleotide for the sequence is valid
+      val nucleotideFromSeq = chArray(posn)
+      if (!possibles.contains(nucleotideFromSeq))
+        throw new IllegalStateException("Unknown nucleotide %s for position %d".format(nucleotideFromSeq, posn))
+      haplotypeList += Mutation(posn, nucleotideFromSeq)
+    }
+
+    haplotypeList.readOnly
+  }
+
+  def eliminateHaplotype(mutation : Mutation, rest : List[String]) = {
+    val posn = mutation.index
+    val nucleotide = mutation.nucleotide
+
+    rest filter { sequence => sequence.charAt(posn) == nucleotide }
+  }
+
+  def findLocalMinimum(handledMutations : List[Mutation], remainingMutations : List[Mutation], rest : List[String]) = {
+
+    val remaining = eliminateHaplotype(handledMutations.head, rest)
+
+    remaining match {
+      case Nil => handledMutations
+      case _ => findLocalMinimum(remainingMutations.head :: handledMutations, remainingMutations.tail, remaining)
+    }
+  }
+
+  def findAllUniqueSignatures(sequence : String, rest : List[String], mutationMap : Map[Int, Set[Char]]) = {
+    if (rest.contains(sequence)) throw new IllegalStateException("Sequence and comparison list must be exclusive")
+
+    val itHap = getHaplotype(sequence, mutationMap)
+
+    val signatures = new ListBuffer[List[Mutation]]
+
+    // run through full list of possible positions
+    for (mutation <- itHap) {
+      val restOfList = itHap - mutation
+
+      signatures.append(findLocalMinimum(List(mutation), restOfList, rest).sort((m1, m2) => (m1.index < m2.index)))
+    }
+
+    val uniques = Set[List[Mutation]]() ++ signatures
+    val uniqueList = List[List[Mutation]]() ++ uniques
+    uniqueList.sort((l1, l2) => l1.length < l2.length)
+  }
+
+}
+
+/**
+ * A lightweight holder for sequence mutation details - the index of the possible mutation, and the specific
+ * nucleotide at that location. Should be extended to include locus name, etc. in the future
+ */
+case class Mutation(val index : Int, val nucleotide : Char)
+
---
+++ test/HaplotyperTest.scala
@@ -0,0 +1,105 @@
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+
+/**
+ * Test for the Haplotyper class
+ */
+
+class HaplotyperTest extends FunSuite with ShouldMatchers {
+
+  val s1 = "gattacca"
+  val s2 = "gatcatca"
+  val s3 = "gat-acca"
+  val s4 = "gattaca"
+
+  val s5 = "cga"
+  val s6 = "caa"
+  val s7 = "aga"
+  val s8 = "cgc"
+
+  val haplotyper = new Haplotyper
+
+  test ("Should not compare different length sequences") {
+    intercept[IllegalArgumentException] {
+      haplotyper.extractMutationSets(s1, s4)
+    }
+  }
+
+  test ("Should return sets of mutations from pair of strings") {
+    val m1 = haplotyper.extractMutationSets(s1, s2)
+    val m2 = haplotyper.extractMutationSets(s1, s3)
+
+    m1 should be (Array(Set('g'), Set('a'), Set('t'), Set('t','c'), Set('a'), Set('c','t'), Set('c'), Set('a')))
+    m2 should be (Array(Set('g'), Set('a'), Set('t'), Set('t','-'), Set('a'), Set('c'), Set('c'), Set('a')))
+  }
+
+  test ("Should return sets of mutations from list of sequences") {
+    val m1 = haplotyper.findAllMutations(List(s1, s2, s3))
+
+    m1 should be (Array(Set('g'), Set('a'), Set('t'), Set('t','c','-'), Set('a'), Set('c','t'), Set('c'), Set('a')))
+  }
+
+  test ("Should not compare list of sequences where some of the sequences differ in length") {
+    intercept[IllegalArgumentException] {
+      haplotyper.findAllMutations(List(s1, s2, s3, s4))
+    }
+  }
+
+  test ("Should return map of mutations only for mutable alleles - odd number of sequences") {
+    val m1 = haplotyper.findAllMutations(List(s1, s2, s3))
+    val mutationMap = haplotyper.mutationMap(m1)
+
+    mutationMap should be (Map(3 -> Set('t','c','-'), 5 -> Set('c', 't')))
+  }
+
+  test ("Should return map of mutations only for mutable alleles - even number of sequences") {
+    val m1 = haplotyper.findAllMutations(List(s1, s2))
+    val mutationMap = haplotyper.mutationMap(m1)
+
+    mutationMap should be (Map(3 -> Set('t','c'), 5 -> Set('c', 't')))
+  }
+
+  test ("Should return map of mutations only for mutable alleles - only one sequences") {
+    val m1 = haplotyper.findAllMutations(List(s1))
+    val mutationMap = haplotyper.mutationMap(m1)
+
+    // should result in empty mutation map
+    mutationMap should be (Map[Int, Set[Char]]())
+  }
+
+  test ("Should return map of mutations only for mutable alleles - empty sequence list") {
+    val m1 = haplotyper.findAllMutations(List[String]())
+    val mutationMap = haplotyper.mutationMap(m1)
+
+    // should result in empty mutation map
+    mutationMap should be (Map[Int, Set[Char]]())
+  }
+
+  test ("Should get correct haplotype combination back from test sequence") {
+    val m1 = haplotyper.findAllMutations(List(s1, s2, s3))
+    val mutationMap = haplotyper.mutationMap(m1)
+
+    val haplotype = haplotyper.getHaplotype("gatcacca", mutationMap)
+
+    haplotype should be (List(Mutation(3,'c'), Mutation(5,'c')))
+  }
+
+  test ("Find Signatures for cgc") {
+    val m1 = haplotyper.findAllMutations(List(s5, s6, s7, s8))
+    val mutationMap = haplotyper.mutationMap(m1)
+
+    val signatures = haplotyper.findAllUniqueSignatures(s8, List(s5, s6, s7), mutationMap)
+
+    println(signatures)
+  }
+
+  test ("Find Signatures for cga with only 3 items") {
+    val m1 = haplotyper.findAllMutations(List(s5, s6, s7))
+    val mutationMap = haplotyper.mutationMap(m1)
+
+    val signatures = haplotyper.findAllUniqueSignatures(s5, List(s6, s7), mutationMap)
+
+    println(signatures)
+  }
+
+}
