6e6d2ded3e7e1bb8eeadc65721e72cac8f1d3169
Alex Furman <alexfurman@Adminstrators-MacBook-Pro.local>
2010-05-05 14:28:28 -0700

    Finished up work on Blaster for now.

---
+++ src/main/scala/com/locusdev/blast/Blaster.scala
@@ -0,0 +1,96 @@
+package com.locusdev.blast
+
+import collection.mutable.ListBuffer
+import java.lang.String
+import java.io.{FileWriter, PrintWriter, FilenameFilter, File}
+
+/**
+ * Given a directory with any number of sequences in the FASTA format, will execute blastn to compare each sequence to
+ * the other, saving the output in the "output" subdirectory.
+ *
+ * Note that for N sequences, this will lead to N^2 executions of balstn and the creation of N^2 files. Depending on sequence
+ * length and N, this may take a long time
+ *
+ * User: alexfurman
+ * Date: May 4, 2010
+ * Time: 12:30:24 PM
+ */
+
+object Blaster {
+  def main(args: Array[String]) {
+    printUsage();
+
+    if (args.length != 1) {
+      System.exit(-1);
+    }
+
+    //get all the .fasta files from the passed in directory
+    val inputDir = new File(args(0))
+    val sequences = inputDir.list.filter {
+      name => name.endsWith(".fasta")
+    };
+
+    //create the output dir if needed
+    val outputDir = new File(inputDir, "blastresults");
+
+    if (!outputDir.exists) {
+      if (!outputDir.mkdirs) {
+        throw new IllegalStateException("Could not create output dir @ " + outputDir)
+      }
+    }
+
+    println("Comparing " + sequences.length + " sequences");
+
+    val resultsSummary = new ListBuffer[String]
+    val resultsDetail = new ListBuffer[String]
+
+    val summaryWriter = new PrintWriter(new FileWriter(new File(outputDir, "resultsSummary.txt")))
+    val detailWriter = new PrintWriter(new FileWriter(new File(outputDir, "resultsDetails.txt")))
+
+    for (x <- 0 until sequences.length) {
+      for (y <- (x + 1) until sequences.length) {
+        val primer1: String = sequences(x).substring(0, sequences(x).indexOf('.'))
+        val primer2: String = sequences(y).substring(0, sequences(y).indexOf('.'))
+        val outName = primer1 + "-" + primer2 + ".out"
+        val command = "/usr/local/ncbi/blast/bin/blastn " +
+                " -query " + inputDir.getAbsolutePath + File.separator + sequences(x) +
+                " -subject " + inputDir.getAbsolutePath + File.separator + sequences(y) + " -outfmt 10 " +
+                "-out " + outputDir + File.separator + outName + " -perc_identity 90";
+        println(command)
+        val process = Runtime.getRuntime.exec(command)
+        process.waitFor();
+        if (process.exitValue() != 0) {
+          throw new IllegalStateException("Failed to execute last command, exit code = " + process.exitValue())
+        }
+
+        //now get the number of rows from the output file, that is out hit score
+        val lines = io.Source.fromFile(outputDir + File.separator + outName).getLines.toList;
+        val summary = primer1 + "\t" + primer2 + "\t" + lines.length
+        summaryWriter.println(summary)
+        summaryWriter.flush
+        println("SUMMARY: " + summary)
+        lines.foreach { line =>
+          val lineString: Any = primer1 + "," + primer2 + "," + line
+          detailWriter.print(lineString)
+          detailWriter.flush
+          print("\tLINE: " + lineString)
+        }
+      }
+    }
+
+    //write out results files
+    summaryWriter.close
+    detailWriter.close
+
+    resultsSummary.foreach {
+      println(_)
+    }
+
+
+  }
+
+  def printUsage() {
+    println("Usage: Blaster targetDir")
+    println("\tOutput files will be saved in /blastresults subdirectory of the target dir")
+  }
+}
--- src/main/scala/com/locusdev/blast/FASTAGenerator.scala
+++ src/main/scala/com/locusdev/blast/FASTAGenerator.scala
@@ -1,6 +1,6 @@
 package com.locusdev.blast
 
-import java.io.File
+import java.io.{PrintWriter, BufferedWriter, FileWriter, File}
 
 /**
  * Takes a Locus Amplicon Sequences spreadsheet and turns it into a bunch of individual FASTA files in a given
@@ -14,7 +14,7 @@ object FASTAGenerator {
   def main(args: Array[String]) {
     printUsage();
 
-    if(args.length != 2){
+    if (args.length != 2) {
       System.exit(-1);
     }
 
@@ -24,11 +24,33 @@ object FASTAGenerator {
       System.exit(-1);
     }
 
+    //check if the specified dir exists and if not try to create it
+    val outdir = new File(args(1))
+    if (!outdir.exists) {
+      if (!outdir.mkdirs) {
+        throw new IllegalStateException("Cannot create output dir @ " + args(1))
+      }
+    }
+
     //open the file
     val lines = io.Source.fromFile(args(0)).getLines;
-    val parsed:List[scala.collection.Map[String, String]] = CSVParser.parse(lines, "\t");
+    val parsed: List[scala.collection.Map[String, String]] = CSVParser.parse(lines, "\t");
 
-    println(parsed)
+    parsed.foreach {
+      line =>
+        val template = line("Template")
+        val primerSet = line("Primer Set")
+        if (template.length != 0 && primerSet.length != 0) {
+          writeFASTA(outdir, primerSet, template)
+        }
+    }
+  }
+
+  def writeFASTA(outdir: File, primerSet: String, sequence: String) = {
+    val writer = new PrintWriter(new FileWriter(new File(outdir, primerSet + ".fasta")))
+    writer.println(">" + primerSet);
+    writer.println(sequence)
+    writer.close
 
   }
 
